5766bca2a65cd148c12bf05e144e23cb56d89557
Jirawat <gbhnewz@gmail.com>
2023-10-03 04:43:54 +0700

    add db function

--- backend-panel/db/Mongos.connect.ts
+++ backend-panel/db/Mongos.connect.ts
@@ -16,6 +16,38 @@ class MongosConnect {
             console.log("DB disconnected successfully to server");
         }
     }
+
+    public async insertOneData(db: string, collection: string, option: any): Promise<any> {
+        const client = new MongoClient(process.env.DATABASE_ENV || '');
+        try {
+            await client.connect()
+            const selectDB = client.db(db)
+            const data = await selectDB.collection(collection).insertOne(option)
+            return data
+        } catch (err: any) {
+            console.log(err);
+            return null
+        } finally {
+            client.close()
+            console.log("DB disconnected successfully to server");
+        }
+    }
+
+    public async insertManyData(db: string, collection: string, option: any): Promise<any> {
+        const client = new MongoClient(process.env.DATABASE_ENV || '');
+        try {
+            await client.connect()
+            const selectDB = client.db(db)
+            const data = await selectDB.collection(collection).insertMany(option)
+            return data
+        } catch (err: any) {
+            console.log(err);
+            return null
+        } finally {
+            client.close()
+            console.log("DB disconnected successfully to server");
+        }
+    }
 }
 
 
