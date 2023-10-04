76db88ccd236d52c5e1b1f83df8d3c9f3fabfee9
realkk715 <realkk715@gmail.com>
2023-10-04 23:56:11 +0700

    add follow brief

--- frontend-panel/src/components/SumStudents.tsx
+++ frontend-panel/src/components/SumStudents.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useContext } from "react";
+import { DataContext } from "../Context";
+
 
 export const SumStudents = () => {
+  const { data }: any = useContext(DataContext);
   return (
-    <div>SumStudents</div>
-  )
-}
+    <div className="pt-40">
+    <table>
+    <thead>
+      <tr>
+        <th>รหัสนักศึกษา</th>
+        <th>ชื่อ-สกุล</th>
+        <th>TH/IS</th>
+        <th>อาจารย์ที่ ปรึกษาหลัก</th>
+        <th>อาจารย์ที่ปรึกษาร่วม</th>
+        <th>ดูจริง(ไหม)?</th>
+        <th>Remark</th>
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((item:any) => (
+        <tr key={item._id.$oid}>
+          <td>{item.student_id.$numberLong}</td>
+          <td>{item.student_name}</td>
+          <td>{item.type}</td>
+          <td>{item.advisor_name}</td>
+          <td>{item.co_advisor_name}</td>
+          <td>{item.semester}</td>
+          <td>{item.Remark}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  </div>
+);
+};
+
