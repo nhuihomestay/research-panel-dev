ad8310da217344a1a9291ff301908dbb307086e6
Jirawat <gbhnewz@gmail.com>
2023-10-05 19:40:01 +0700

    Revert "Merge branch 'sunvo' into develop"
    This reverts commit c1bbc05eb3c4daa05851a591a54408f8b56fe6b2, reversing
    changes made to bd43b445f3deac7c79b790357d2e074b99414e53.

---
+++ backend-panel/src/constant/db.constant.ts
@@ -0,0 +1,14 @@
+enum DATABASE_NAME {
+    DEV_DB = 'research-panel-dev',
+    DB = 'research-panel',
+}
+
+enum COLLECTION_NAME {
+    ADVISOR = 'advisor',
+    ADVISOR_TEST = 'advisor-test',
+    STUDENT = 'student',
+    STUDENT_TEST = 'student-test',
+    USER = 'user'
+}
+
+export { DATABASE_NAME, COLLECTION_NAME }
