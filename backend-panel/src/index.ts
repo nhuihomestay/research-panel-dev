36bf79efe0cc93627052b410351f69c6dbe986e9
Jirawat <gbhnewz@gmail.com>
2023-10-06 22:04:43 +0700

    fix into this shit

--- backend-panel/src/index.ts
+++ backend-panel/src/index.ts
@@ -1,8 +1,19 @@
-import app from './server';
-import './LoadEnv';
+// import app from './server';
+// import './LoadEnv';
 
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, () => {
+//     console.log('Simple project server started on port: ' + port);
+// });
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const server_1 = tslib_1.__importDefault(require("./server"));
+require("./LoadEnv");
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
+server_1.default.listen(port, () => {
     console.log('Simple project server started on port: ' + port);
 });
+//# sourceMappingURL=index.js.map
