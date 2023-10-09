30dc1b1805ef0f02258f813647b7a8c18f690c98
Jirawat <gbhnewz@gmail.com>
2023-10-09 20:57:26 +0700

    add loglevel

--- backend-panel/src/routes/advisor.route.ts
+++ backend-panel/src/routes/advisor.route.ts
@@ -1,20 +1,24 @@
 import { Request, Response, Router } from 'express';
 import { AdvisorCtr } from '@ctr';
+import log from 'loglevel';
 const router = Router()
 
 router.get('/', async (req: Request, res: Response) => {
+    log.warn('req:', req.body)
     const advisorCtr = new AdvisorCtr()
     const result = await advisorCtr.getAdvisor(req.body)
     res.json(result)
 })
 
 router.post('/add', async (req: Request, res: Response) => {
+    log.warn('req:', req.body)
     const advisorCtr = new AdvisorCtr()
     const result = await advisorCtr.addAdvisor(req.body)
     res.json(result)
 })
 
 router.post('/check', async (req: Request, res: Response) => {
+    log.warn('req:', req.body)
     const advisorCtr = new AdvisorCtr()
     const result = await advisorCtr.checkThesis(req.body)
     res.json(result)
--- backend-panel/src/routes/student.route.ts
+++ backend-panel/src/routes/student.route.ts
@@ -1,26 +1,31 @@
 import { Request, Response, Router } from 'express';
 import { StudentCtr } from '@ctr';
+import log from 'loglevel';
 const router = Router()
 
 router.get('/', async (req: Request, res: Response) => {
+    log.warn('req:', req.body)
     const studentCtr = new StudentCtr()
     const result = await studentCtr.getStudent(req.body)
     res.json(result)
 })
 
 router.post('/add', async (req: Request, res: Response) => {
+    log.warn('req:', req.body)
     const studentCtr = new StudentCtr()
     const result = await studentCtr.addStudent(req.body)
     res.json(result)
 })
 
 router.put('/update', async (req: Request, res: Response) => {
+    log.warn('req:', req.body)
     const studentCtr = new StudentCtr()
     const result = await studentCtr.updateStudent(req.body)
     res.json(result)
 })
 
 router.get('/count', async (req: Request, res: Response) => {
+    log.warn('req:', req.body)
     const studentCtr = new StudentCtr()
     const result = await studentCtr.groupStudent(req.body)
     res.json(result)
